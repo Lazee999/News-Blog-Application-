27bfd656adcb8dd1ef45e5a68e3d6e57642347f7
Lasith Banuka <lasithbanuka110@gmail.com>
2025-03-04 21:17:51 +0300

    Navigate function add

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import News from './Components/News/News'
 import Blog from './Components/Blog/Blog'
 
 const App = () => {
+  const [showNews, setShowNews] = useState(true)
+  const [showBlog, setShowBlog] = useState(false)
+  
+  const handleToggleBlogs = () => {
+    setShowNews(false)
+    setShowBlog(true)
+  }
+
+  const handleToggleNews = () => {
+    setShowNews(true)
+    setShowBlog(false)
+  }
+  
   return (
     <div className='container'>
       <div className="news-blog-app">
-        {/* <News /> */}
+        {showNews &&  <News onShowBlogs={handleshowBlogs} />}
+        {showBlog &&  <Blog onClick={handleBackToNews} />}
+        <News /> 
         <Blog />
       </div>
     </div>
