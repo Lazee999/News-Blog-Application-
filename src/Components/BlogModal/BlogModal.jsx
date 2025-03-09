03e10ef9587b3d657109aa9c38bbd43e8d1e7a06
Lasith Banuka <lasithbanuka110@gmail.com>
2025-03-09 19:51:11 +0300

    update blog modal styles

--- src/Components/BlogModal/BlogModal.jsx
+++ src/Components/BlogModal/BlogModal.jsx
@@ -1,19 +1,24 @@
-import React from 'react'
-import demoImg from '../../assets/images/demo.jpg'
-import './BlogModal.scss'
-const BlogModal = () => {
+import React from 'react';
+import demoImg from '../../assets/images/demo.jpg';
+import './BlogModal.scss';
+
+const BlogModal = ({ onClose }) => {
   return (
     <div className="modal-overlay">
-        <span className="close-content">
-            <i className='fa-sold fa-xmark'></i>
+      <div className="blog-modal">
+        <span className="close-content" onClick={onClose}>
+          <i className="fa-solid fa-xmark"></i>
         </span>
-        <img src={demoImg} alt="Modal Image" className='blogs-modal-image'/>
-        <h2 className='blogs-modal-title'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt explicabo ullam quos esse consectetur, nihil, exercitationem nostrum magni, ipsum dignissimos ipsam veritatis. Quam itaque cum officiis aperiam, minima recusandae accusantium!</h2>
-        <p className='blog-post-content'> 
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit deleniti quo ipsum reiciendis voluptatibus nulla exercitationem culpa praesentium ipsam eum, placeat eveniet. Quibusdam doloribus cum natus velit eveniet vel nihil!
+        <img src={demoImg} alt="Blog Image" className="blogs-modal-image" />
+        <h2 className="blogs-modal-title">Lorem, ipsum dolor sit amet consectetur adipiscing elit.</h2>
+        <p className="blog-post-content">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum, corrupti. Voluptas aliquid, facilis esse.
+          Temporibus tempora repellat ipsam, dolorum cum excepturi quibusdam. 
+          Consequatur molestias molestiae dolore minima quod?
         </p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default BlogModal
+export default BlogModal;
