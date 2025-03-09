7952c8ddfc472b6f7f180d9d9d766f336696de03
Lasith Banuka <lasithbanuka110@gmail.com>
2025-03-09 20:09:02 +0300

    update blog modal

--- src/Components/BlogModal/BlogModal.jsx
+++ src/Components/BlogModal/BlogModal.jsx
@@ -2,20 +2,22 @@ import React from 'react';
 import demoImg from '../../assets/images/demo.jpg';
 import './BlogModal.scss';
 
-const BlogModal = ({ onClose }) => {
+const BlogModal = ({ show,blog, onClose }) => {
+    if (!show) {
+        return null;
+    }
+
   return (
     <div className="modal-overlay">
       <div className="blog-modal">
         <span className="close-content" onClick={onClose}>
           <i className="fa-solid fa-xmark"></i>
         </span>
-        <img src={demoImg} alt="Blog Image" className="blogs-modal-image" />
-        <h2 className="blogs-modal-title">Lorem, ipsum dolor sit amet consectetur adipiscing elit.</h2>
-        <p className="blog-post-content">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum, corrupti. Voluptas aliquid, facilis esse.
-          Temporibus tempora repellat ipsam, dolorum cum excepturi quibusdam. 
-          Consequatur molestias molestiae dolore minima quod?
-        </p>
+        {blog.image &&  <img src={blog.image} alt={blog.title} className="blogs-modal-image" />}
+       
+        <h2 className="blogs-modal-title">   {blog.title} </h2>
+     
+        <p className="blog-post-content">{blog.content}</p>
       </div>
     </div>
   );
