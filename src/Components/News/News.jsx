ee1f9f1a66b0ad219eb4204ae3b4163af21bfe4b
Lasith Banuka <lasithbanuka110@gmail.com>
2025-03-09 20:04:27 +0300

    display blog post

--- src/Components/News/News.jsx
+++ src/Components/News/News.jsx
@@ -34,6 +34,8 @@ const News = ({ onShowBlogs, blogs }) => {
     const [selectedArticle, setSelectedArticle] = useState(null);
     const [bookmarks, setBookmarks] = useState([]);
     const [showBookmarkModal, setShowBookmarkModal] = useState(false);
+    const [selectedPost, setSelectedPost] = useState(null);
+    const [showBlogModal, setShowBlogModal] = useState(false);
 
     useEffect(() => {
         const fetchNews = async () => {
@@ -89,6 +91,17 @@ const News = ({ onShowBlogs, blogs }) => {
             return updatedBookmarks
         });
     };
+
+
+    const handleBlogClick = (blog) => {
+        setSelectedPost(blog);
+        setShowBlogModal(true);
+    }
+
+    const closeBlogModal = () => {
+        setShowBlogModal(false);
+        setSelectedPost(null);
+    }
 
     return (
         <div className='news'>
@@ -197,7 +210,7 @@ const News = ({ onShowBlogs, blogs }) => {
                     <h1 className="my-blogs-heading"> My Blogs</h1>
                     <div className="blog-posts">
                         {blogs.map((blog, index) => (
-                            <div key={index} className="blog-post">
+                            <div key={index} className="blog-post" onClick={() => handleBlogClick(blog)}>
                                 <img src={blog.image || noImg} alt={blog.title} />
                                 <h3> {blog.title}</h3>
                                 {/* <p> {blog.content}</p> */}
@@ -210,7 +223,10 @@ const News = ({ onShowBlogs, blogs }) => {
                             </div>
                         ))}
                     </div>
-                    <BlogModal />
+                    {selectedPost && showBlogModal && (
+                         <BlogModal show={showBlogModal} blogs={selectedPost} onClose={closeBlogModal}/>
+                    )}
+                   
                 </div>
                 <div className="weather-calendar">
                     <Weather />
